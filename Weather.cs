10694f4cea239f5ad8fc17ba3168adf4e37b1e24
unknown <jeffrey.r.carter86@gmail.com>
2020-04-20 16:04:08 -0400

    Populated weather and customer classes, added varibles and spawners to player class

--- Weather.cs
+++ Weather.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace LemonadeStand_3DayStarter
 {
-    class Weather
+    public class Weather
     {
+        public string condition;
+        public int temperature;
+        List<string> weatherConditions;
+        public string predictedForecast;
+        Random random;
+        public Weather()
+        {
+            random = new Random();
+            weatherConditions = new List<string>() { "Sunny", "Overcast", "Hazy", "Rain" };
+            SetTemperature();
+            SetCondition(GenerateRandomIndex());
+            SetForecast();
+        }
+
+        public void SetTemperature()
+        {
+                  
+            temperature = random.Next(50, 101);
+        }
+        public void SetCondition(int index)
+        {
+            
+            condition = weatherConditions[index];
+        }
+        public void SetForecast()
+        {
+            
+            int temperatureVariance = random.Next(-7, 8);
+            predictedForecast = (temperature + temperatureVariance) + " degrees and " + condition;
+        }
+        public int GenerateRandomIndex()
+        {
+            return random.Next(weatherConditions.Count);
+        }
+
+
     }
 }
